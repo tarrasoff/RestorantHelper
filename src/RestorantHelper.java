845e1922202ea0cae8c96692918b990781f612c1
TarasovAlex <tarasof.av@gmail.com>
2023-02-22 18:54:07 +0200

    Initial commit

---
+++ src/RestorantHelper.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class RestorantHelper {
+
+    public static void main(String[] args) throws IOException {
+      Set <String> products = new HashSet<>();
+      BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Please invite products to buy: ");
+      while(true){
+          String product = reader.readLine();
+          if(product.equals("finish")){
+              break;
+          }
+          products.add(product);
+      }
+      while(!products.isEmpty()){
+          System.out.println("Left to buy: ");
+          for (String product : products){
+              System.out.println(product);
+          }
+          System.out.println("Please wright product to remove: ");
+          String product = reader.readLine();
+          if(products.contains(product)) {
+              products.remove(product);
+          } else {
+              System.out.println("No this product in the list");
+          }
+
+      }
+        System.out.println("Congratulations you buy all products");
+
+
+
+    }
+
+}
